be903ffb0be578ab20ab2758011545e2b29a5eca
Jansen Smith <jansen@bstarengineering.com>
2025-08-21 22:50:59 -0400

    export slices

--- ArtBorders.groovy
+++ ArtBorders.groovy
@@ -2,6 +2,9 @@ import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
 import eu.mihosoft.vrl.v3d.Cylinder
 import eu.mihosoft.vrl.v3d.Transform
+import eu.mihosoft.vrl.v3d.svg.SVGExporter
+import eu.mihosoft.vrl.v3d.Polygon
+import eu.mihosoft.vrl.v3d.Slice
 
 def name
 def piece_width
@@ -41,7 +44,20 @@ if(args==null){
 		"https://github.com/JansenSmith/ArtBorders.git", // git location of the library
 		  "ArtBorders.groovy" , // file to load
 		  borders_rabbet_true_params // send the factory the name param
-	)
+	)//.movex(10).movey(10)
+		
+	CSG cutout = new Cube(piece_width, piece_height, border_thickness).toCSG()
+					.toXMin()
+					.toYMin()
+					.toZMin()
+	CSG ziggurat = borders_rabbet_true.union(cutout).union(cutout.movez(cutout.totalZ/2))
+	ziggurat = ziggurat.addSlicePlane(new Transform().movez(ziggurat.totalZ - 0.1))
+					.addSlicePlane(new Transform().movez(0.1).movey(ziggurat.totalY + 10))
+					.addSlicePlane(new Transform().movez(ziggurat.totalZ/2).movey(2*(ziggurat.totalY + 10)))
+	ziggurat = ziggurat.addExportFormat("svg")
+	
+	
+	
 	borders_rabbet_false = borders_rabbet_false.setColor(javafx.scene.paint.Color.DARKGRAY)
 		.setName(name+"_base")
 		.addAssemblyStep(0, new Transform())
@@ -53,15 +69,28 @@ if(args==null){
 		})
 		
 	borders_rabbet_true = borders_rabbet_true.setColor(javafx.scene.paint.Color.DARKRED)
-		.setName(name+"_base")
+		.setName(name+"_base_rabbeted")
 		.addAssemblyStep(0, new Transform())
 		.setManufacturing({ toMfg ->
 			return toMfg
 					//.rotx(180)// fix the orientation
 					//.toZMin()//move it down to the flat surface
 		})
-		
-	return borders_rabbet_true//, borders_rabbet_false]
+	
+	ziggurat = ziggurat.setColor(javafx.scene.paint.Color.MAGENTA)
+		.setName(name+"_ziggurat")
+//		.addAssemblyStep(0, new Transform())
+//		.setManufacturing({ toMfg ->
+//			return toMfg
+//					//.rotx(180)// fix the orientation
+//					//.toZMin()//move it down to the flat surface
+//		})
+		//.addSlicePlane(new Transform().movez(-ziggurat.totalZ+0.1))
+	
+	printSingleExample(piece_width, piece_height)
+	//ret = [borders_rabbet_true.movez(-border_thickness/2-0.1+10)]
+	ret = ziggurat//.roty(45)//.movez(-ziggurat.totalZ+0.1)
+	return ret
 } else if (args.size() >= 5 && args.get(5)) {
 	name = args.get(0)
 	piece_width = args.get(1)
@@ -105,8 +134,9 @@ if(args==null){
 		  "ArtBorders.groovy" , // file to load
 		  border_external_params // send the factory the name param
 	)
-	ret = border_internal.union(border_external)
-	return ret
+	border = border_internal.union(border_external)
+	ret = border
+	return ret //[ret, cutout]
 } else {
 	name = args.get(0)
 	piece_width = args.get(1)
@@ -155,10 +185,75 @@ CSG cutout = new Cube(width, height, border_thickness).toCSG()
 					
 borders = borders.difference(cutout)
 
-if(do_rabbet) {
-	
-}
-
 CSG ret = borders
 		
 return ret
+
+/**
+ * Calculates reasonable backboard dimensions for framing
+ * @param piece_width Width of artwork in mm
+ * @param piece_height Height of artwork in mm
+ * @return Map with backboard_x and backboard_y dimensions in mm
+ */
+def calculateBackboardSize(double piece_width, double piece_height) {
+	
+	// Frame parameters (all in mm)
+	def frame_width = 20.0           // Frame molding width on each side
+	def cutting_margin = 6.0         // Laser cutting margin on each side
+	
+	// Mat border calculation - dynamic based on artwork size
+	def base_mat_border = 50.0       // Minimum mat border
+	def size_factor = Math.max(piece_width, piece_height) / 200.0  // Scale factor
+	def mat_border = Math.max(base_mat_border, base_mat_border * size_factor)
+	
+	// Optional: bottom-weighted mat (traditional framing)
+	def mat_top = mat_border
+	def mat_bottom = mat_border * 1.15  // 15% larger bottom border
+	def mat_left = mat_border
+	def mat_right = mat_border
+	
+	// Calculate total backboard dimensions
+	def backboard_x = piece_width + mat_left + mat_right + (frame_width * 2) + (cutting_margin * 2)
+	def backboard_y = piece_height + mat_top + mat_bottom + (frame_width * 2) + (cutting_margin * 2)
+	
+	// Round up to nearest mm for practical cutting
+	backboard_x = Math.ceil(backboard_x)
+	backboard_y = Math.ceil(backboard_y)
+	
+	return [
+		x: backboard_x,
+		y: backboard_y,
+		mat_border: mat_border,
+		frame_info: [
+			piece_width: piece_width,
+			piece_height: piece_height,
+			mat_top: mat_top,
+			mat_bottom: mat_bottom,
+			mat_left: mat_left,
+			mat_right: mat_right,
+			frame_width: frame_width,
+			cutting_margin: cutting_margin
+		]
+	]
+}
+
+/**
+ * Prints detailed info for a single backboard calculation
+ */
+def printSingleExample(double piece_width, double piece_height) {
+    def backboard = calculateBackboardSize(piece_width, piece_height)
+    println "Artwork: ${backboard.frame_info.piece_width}mm x ${backboard.frame_info.piece_height}mm"
+    println "Backboard needed: ${backboard.x}mm x ${backboard.y}mm"
+    println "Mat border: ${Math.round(backboard.mat_border)}mm (bottom: ${Math.round(backboard.frame_info.mat_bottom)}mm)"
+}
+
+/**
+ * Prints a table of multiple size examples
+ */
+def printMultipleSizeExamples() {
+    println "\n--- Multiple Size Examples ---"
+    [[100, 150], [250, 150], [300, 200], [400, 300]].each { dims ->
+        def test = calculateBackboardSize(dims[0], dims[1])
+        println "${dims[0]}x${dims[1]}mm artwork => ${test.x}x${test.y}mm backboard"
+    }
+}
